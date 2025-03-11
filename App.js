cdac258536292dfcdb528762772b9329f8fc7135
Matheus <maatheus.vinicius000@gmail.com>
2025-03-10 22:48:42 -0300

    Aula exemplo 01

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 import Constants from 'expo-constants';
+
+// Exemplos
+import Exemplo01 from './src/exemplos/exemplo-01';
+
+// Atividades
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Aula react </Text>
-      <StatusBar style="auto" />
+      <Exemplo01 />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,11 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+   // alignItems: 'center',
+    // justifyContent: 'center',
     padding: 8,
-    margintTop: Constants.StatusBarHeight || 8,
+    paddingTop: Constants.statusBarHeight || 8,
     
   },
 });
